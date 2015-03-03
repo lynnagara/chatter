ff3b069102d5bcfe07b859741149259987cc4795
Lyn Nagara <lyn.nagara@gmail.com>
2015-03-03 13:40:22 -0500

    handle websocket responses

--- apps/client/js/app/utils/webapi.js
+++ apps/client/js/app/utils/webapi.js
@@ -8,17 +8,21 @@ class WebAPIUtils {
 
 	connectWebsocket(username) {
 		var ws = new WebSocket('ws://' + location.host);
-		ws.onopen = function(event) {
+		ws.onopen = (event) => {
 			// Check if the username is valid
 			ws.send(JSON.stringify({username: username, action: 'setUsername'}));
 
 			var act = new Actions();
 			act.userConnectionSuccess();
 		};
-		ws.onerror = function(error) {
+		ws.onerror = (error) => {
 			var act = new Actions();
 			act.userConnectionFailure();
 		};
+		ws.onmessage = (message) =>{
+			console.log(message.data);    
+		}
+
 
 	}
 
